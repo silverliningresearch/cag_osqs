04f281d4c23f3c051a0625ba77fae5f77e099172
lexuanbinh <lb@silverliningresearch.com>
2023-08-13 12:08:48 +0700

    update files

---
+++ data/interviews_count.js
@@ -0,0 +1,30 @@
+let interview_statistics  = `[
+    {
+        "InterviewDate": "2023-08-13",
+        "quota_id": "4 Fingers Crispy Chicken\u2666TERMINAL 1\u2666Airside\u266603-47\u2666FBA\u2666Min6",
+        "Number of interviews": 1,
+        "download_time": "13-08-2023 12:37:00"
+    },
+    {
+        "InterviewDate": "2023-08-13",
+        "quota_id": "Burger King\u2666TERMINAL 1\u2666Airside\u266603-02\u2666FBA\u2666Min6",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-08-13",
+        "quota_id": "Crystal Jade La Mian Xiao Long Bao\u2666TERMINAL 1\u2666Airside\u266603-54\u2666FBA\u2666Min6",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-08-13",
+        "quota_id": "Starbucks Coffee\u2666TERMINAL 4\u2666Landside\u2666#02-04\u2666FBL\u2666Min6",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2023-08-13",
+        "quota_id": "Ya Kun\u2666TERMINAL 1\u2666Airside\u266602-K19\u2666FBA\u2666Min6",
+        "Number of interviews": 1
+    }
+]    
+
+ `;
--- scripts/PrepareData.js
+++ scripts/PrepareData.js
@@ -1,43 +1,51 @@
 var quota_data;
 var download_time;
-var total_quota = 2500;
+var total_quota = 4800;
 var total_completed;
 var total_remaining;
-var hard_quota = 2248;
-var hard_quota_completed;
-var hard_quota_remaining;
+var minium_quota = 0;
+var minium_quota_completed;
+var minium_quota_remaining;
 var felxible_quota_remaining;
 /************************************/
 function prepareData() {
-  raw_data = JSON.parse(weekly_plan);
- 
+  quota_data = JSON.parse(quota_list);
+  var interview_data  = JSON.parse(interview_statistics);
+
+  download_time = interview_data[0].download_time;
+
   var i = 0;
   total_completed = 0;
-  hard_quota_completed = 0;
-
-  quota_data = [];
-  quota_data.length = 0;
-  for (i = 0; i<raw_data.length; i++ )
+  minium_quota_completed = 0;
+  minium_quota = 0;
+  for (i = 0; i<quota_data.length; i++ )
   {
-    var item = raw_data[i];
-    if (item.Completed) 
-    {
-      total_completed = total_completed + item.Completed;
-    }
+    var quota = quota_data[i];
+    minium_quota = minium_quota + quota.Quota;
 
-    if (item.Completed > item.Quota) {
-      hard_quota_completed = hard_quota_completed + item.Quota;
+    quota.Completed  = 0;
+    for (j = 0; j<interview_data.length; j++ ) {
+
+      interview = interview_data[j];
+      console.log("interview",interview );
+      console.log("quota.quota_id",quota.quota_id );
+      if ( quota.quota_id == interview.quota_id) {
+        quota.Completed = quota.Completed + interview["Number of interviews"];
+
+      }
+    }
+    total_completed = total_completed + quota.Completed  ;
+    quota.Missing = quota.Quota - quota.Completed;
+    
+    if (quota.Completed > quota.Quota) {
+      minium_quota_completed = minium_quota_completed + quota.Quota;
     }
     else {
-      hard_quota_completed = hard_quota_completed + item.Completed;
-    }
-
-    if (item.Missing > 0) 
-    {
-      quota_data.push(item);
+      minium_quota_completed = minium_quota_completed + quota.Completed;
     }
   }
+
   total_remaining = total_quota - total_completed;
-  hard_quota_remaining = hard_quota - hard_quota_completed;
-  felxible_quota_remaining = total_remaining - hard_quota_remaining;
+  minium_quota_remaining = minium_quota - minium_quota_completed;
+  felxible_quota_remaining = total_remaining - minium_quota_remaining;
 }
